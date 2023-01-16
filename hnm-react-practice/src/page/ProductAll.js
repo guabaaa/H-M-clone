901c1f8cbe036a57dac7392e2206c74d80868bb0
guabaaa <gdandtop950923@gmail.com>
2023-01-17 01:01:43 +0900

    npm start 오류 중 중간 올림

---
+++ hnm-react-practice/src/page/ProductAll.js
@@ -0,0 +1,16 @@
+import React, { useEffect } from "react";
+
+const ProductAll = () => {
+  const getProducts = async () => {
+    let url = "http://localhost:5000/products";
+    let response = await fetch(url);
+    let data = await response.json();
+    console.log(data);
+  };
+  useEffect(() => {
+    getProducts();
+  }, []);
+  return <div></div>;
+};
+
+export default ProductAll;
